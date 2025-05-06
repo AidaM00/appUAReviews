c95bfb3fade0e5ca7954c5e131d71dcdb128d469
AidaM00 <mateo.128130@e.unavarra.es>
2025-05-06 13:53:04 +0200

    Pruebas de conexión

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>UA Reviews!!</Text>
+      <Text>Prueba de conexión</Text>
       <StatusBar style="auto" />
     </View>
   );
