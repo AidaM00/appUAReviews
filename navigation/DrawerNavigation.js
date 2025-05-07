fe0d00fa6a9b915cdd44afdeaab74cb6fb80fec6
AidaM00 <mateo.128130@e.unavarra.es>
2025-05-07 16:45:47 +0200

    Barra navegación con iconos

--- navigation/DrawerNavigation.js
+++ navigation/DrawerNavigation.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Ionicons';
 import Home from '../components/Home';
 import Apartments from '../components/Apartments';
 import Statistics from '../components/Statistics';
@@ -12,10 +13,42 @@ export default function DrawerNavigation() {
   return (
     <NavigationContainer>
       <Drawer.Navigator initialRouteName="Inicio">
-        <Drawer.Screen name="Inicio" component={Home} />
-        <Drawer.Screen name="Apartamentos" component={Apartments} />
-        <Drawer.Screen name="Estadísticas" component={Statistics} />
-        <Drawer.Screen name="Mi cuenta" component={Account} />
+        <Drawer.Screen
+          name="Inicio"
+          component={Home}
+          options={{
+            drawerIcon: ({ color, size }) => (
+              <Icon name="home-outline" color={color} size={size} />
+            ),
+          }}
+        />
+        <Drawer.Screen
+          name="Apartamentos"
+          component={Apartments}
+          options={{
+            drawerIcon: ({ color, size }) => (
+              <Icon name="business-outline" color={color} size={size} />
+            ),
+          }}
+        />
+        <Drawer.Screen
+          name="Estadísticas"
+          component={Statistics}
+          options={{
+            drawerIcon: ({ color, size }) => (
+              <Icon name="bar-chart-outline" color={color} size={size} />
+            ),
+          }}
+        />
+        <Drawer.Screen
+          name="Mi cuenta"
+          component={Account}
+          options={{
+            drawerIcon: ({ color, size }) => (
+              <Icon name="person-outline" color={color} size={size} />
+            ),
+          }}
+        />
       </Drawer.Navigator>
     </NavigationContainer>
   );
